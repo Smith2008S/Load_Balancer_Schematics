ac97a94e602a7e4960d208d83befec03a6e79e87
Smith2008S <60987042+Smith2008S@users.noreply.github.com>
2020-06-25 19:12:17 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 variable "iaas_classic_username" {}
 #variable "iaas_classic_api_key" {}
 # variable "region" {}
-variable "ibmcloud_api_key" {}
+# variable "ibmcloud_api_key" {}
 
 provider "ibm" {
   iaas_classic_username = "var.iaas_classic_username"
  # iaas_classic_api_key  = "var.iaas_classic_api_key"
 #  region                = "var.region"
-  ibmcloud_api_key      = "var.ibmcloud_api_key"
+  ibmcloud_api_key      = "Hm74xDW9_yXqdEbXVg3je0sco641A6_5vcdcHSY3UblV"# "var.ibmcloud_api_key"
 }
